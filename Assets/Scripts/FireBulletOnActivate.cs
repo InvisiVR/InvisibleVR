3ef0239f5de1c57e6660c79bf8db0560f7b2d4b8
hyeon <urim2270@naver.com>
2023-09-16 23:47:23 +0900

    XR development#9
    Bullet Fire dir update

--- Assets/Scripts/FireBulletOnActivate.cs
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -8,7 +8,7 @@ public class FireBulletOnActivate : MonoBehaviour
 
     public GameObject bullet;
     public Transform spawnPoint;
-    public float fireSpeed = 20;
+    public float fireSpeed = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +21,8 @@ public class FireBulletOnActivate : MonoBehaviour
     {   
         GameObject spawnedBullet = Instantiate(bullet);
         spawnedBullet.transform.position = spawnPoint.position;
-        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        spawnedBullet.transform.rotation = spawnPoint.rotation;
+        spawnedBullet.GetComponentInChildren<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
         Destroy(spawnedBullet, 5);
     }
 }
