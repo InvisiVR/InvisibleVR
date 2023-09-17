f18ae5f299160a39a2c8e8a05193b6c28fd24411
hyeon <urim2270@naver.com>
2023-09-17 22:42:47 +0900

    XR development#15
    Vinette & Reload Update

--- Assets/Scripts/FireBulletOnActivate.cs
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -5,20 +5,25 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class FireBulletOnActivate : MonoBehaviour
 {
+    [Header("Prefab References")]
     public GameObject bullet;
     public GameObject cartridge;
 
+    [Header("ParticleSystem References")]
     public ParticleSystem MuzzleFlashParticle;
 
+    [Header("Location References")]
     public Transform bulletSpawnPoint;
     public Transform cartridgeSpawnPoint;
 
-    
+    public Animator shotAnime;
 
-    public Animator anime;
-
+    [Header("Settings")]
     public float fireSpeed = 400f;
     public float cartridgeSpeed = 5f;
+
+    public AudioSource source;
+    public AudioClip fireSound;
 
     // Start is called before the first frame update
     void Start()
@@ -29,24 +34,35 @@ public class FireBulletOnActivate : MonoBehaviour
 
     public void FireBullet(ActivateEventArgs arg)
     {   
+        shotAnime.SetTrigger("doShot");
+    }
+
+    void Shoot()
+    {
+        source.PlayOneShot(fireSound);
+
         GameObject spawnedBullet = Instantiate(bullet);
-        GameObject spawnedCartridge = Instantiate(cartridge);
 
         spawnedBullet.transform.position = bulletSpawnPoint.position;
         spawnedBullet.transform.rotation = bulletSpawnPoint.rotation;
 
         MuzzleFlashParticle.Play();
 
+        spawnedBullet.GetComponentInChildren<Rigidbody>().velocity = bulletSpawnPoint.forward * fireSpeed;
+
+        Destroy(spawnedBullet, 5);
+    }
+
+    void CasingRelease()
+    {
+        GameObject spawnedCartridge = Instantiate(cartridge);
+
         spawnedCartridge.transform.position = cartridgeSpawnPoint.position;
         spawnedCartridge.transform.rotation = cartridgeSpawnPoint.rotation;
 
-        anime.SetTrigger("doShot");
-
-        spawnedBullet.GetComponentInChildren<Rigidbody>().velocity = bulletSpawnPoint.forward * fireSpeed;
         spawnedCartridge.GetComponentInChildren<Rigidbody>().velocity = new Vector3(1, Random.Range(1f, 1.5f), Random.Range(-1.5f, -1f)) * Random.Range(0.5f, 1f);
         spawnedCartridge.GetComponentInChildren<Rigidbody>().angularVelocity = new Vector3(Random.Range(1f, 1.5f), Random.Range(1f, 1.5f), Random.Range(-1.5f, -1f));
 
-        Destroy(spawnedBullet, 5);
         Destroy(spawnedCartridge, 8);
     }
 }
