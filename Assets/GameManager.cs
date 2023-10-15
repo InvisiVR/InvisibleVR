ab193e24a46be6093638a9a4fc2c08d9ed3f2f5e
hyeon <urim2270@naver.com>
2023-10-16 00:38:56 +0900

    Intro Scene & Game Manager Update

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //반응형 시간 시스템 정보
+    public int curTime;//DateTime.Now.ToString(); 초 단위 계산
+    public int expectPlayTime; //초 단위 계산
+
+    //key & 단서 획득 정보
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
