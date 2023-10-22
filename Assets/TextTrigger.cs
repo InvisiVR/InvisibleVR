038fcf270e15674177d88b67a4ddbfe29c0ba676
hyeon <urim2270@naver.com>
2023-10-23 02:08:38 +0900

    Dialogue System Complete #8

--- Assets/TextTrigger.cs
+++ Assets/TextTrigger.cs
@@ -2,21 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TriggerType { localtionTrigger, InteractionTrigger, EncounterTrigger, DodgeTrigger }
+
 public class TextTrigger : MonoBehaviour
 {
-    public int TextID;
-
-    bool isTriggered = false;
+    public TriggerType triggerType;
+    public int textID;
+    public float delay;
+    public bool isTriggered;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!isTriggered)
+        if(triggerType == TriggerType.localtionTrigger)
         {
-            if (other.tag == "Player")
+            if (!isTriggered)
             {
-                DialogueManager.instance.CallDialogue(TextID);
-                isTriggered = true;
+                if (other.tag == "Player")
+                {
+                    StartCoroutine(TriggerDelay());
+
+                    isTriggered = true;
+                }
             }
         }
+    }
+
+    public void InteractionTriggerTriggered()
+    {
+        if(triggerType == TriggerType.InteractionTrigger)
+        {
+            if (!isTriggered)
+            {
+                isTriggered = true;
+
+                StartCoroutine(TriggerDelay());
+            }
+        }
+    }
+
+    public IEnumerator TriggerDelay()
+    {
+        yield return new WaitForSeconds(delay);
+
+        DialogueManager.instance.CallDialogue(textID);
     }
 }
