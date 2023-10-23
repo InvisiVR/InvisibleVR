a8c135570d6f598a843c91af16334916a6631dfe
Jerry <50892930+lsjsos@users.noreply.github.com>
2023-10-23 21:35:55 +0900

    Map Scene #B1_LightProduction2

--- Assets/Scripts/FlickeringLight.cs
+++ Assets/Scripts/FlickeringLight.cs
@@ -11,6 +11,9 @@ public class FlickeringLight : MonoBehaviour
     public float minTime;
     public float maxTime;
     public float timer;
+    public Renderer light_render;
+    public Renderer light_render1;
+    public int enterCount = 0;
 
     public AudioSource aus;
     public AudioClip lightAudio;
@@ -21,27 +24,43 @@ public class FlickeringLight : MonoBehaviour
     void Start()
     {
         timer = Random.Range(minTime, maxTime);
+        light_render.material.EnableKeyword("_EMISSION");
+        light_render1.material.EnableKeyword("_EMISSION");
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && enterCount==0)
         {
             FlickerLight();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        light_render.material.SetColor("_EmissionColor", new Color32(191, 171, 138, 255));
+        light_render1.material.SetColor("_EmissionColor", new Color32(191, 171, 138, 255));
+        light1.GetComponent<Light>().color = new Color32(241, 208, 209, 255);
+        light2.GetComponent<Light>().color = new Color32(241, 208, 209, 255);
+        light1.enabled = true;
+        light2.enabled = true;
+        enterCount++;
     }
 
     void FlickerLight()
     {
         light1.GetComponent<Light>().color = Color.red;
         light2.GetComponent<Light>().color = Color.red;
-
+        light_render.material.SetColor("_EmissionColor", Color.red);
+        light_render1.material.SetColor("_EmissionColor", Color.red);
         if (timer > 0)
             timer -= Time.deltaTime;
         if (timer < 0)
         {
             light1.enabled = !light1.enabled;
             light2.enabled = !light2.enabled;
+            light_render.material.SetColor("_EmissionColor", Color.black);
+            light_render1.material.SetColor("_EmissionColor", Color.black);
             timer = Random.Range(minTime, maxTime);
             aus.PlayOneShot(lightAudio);
         }
