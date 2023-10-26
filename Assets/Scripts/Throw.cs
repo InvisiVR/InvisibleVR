746418213272cd7f254823e9ea557df4d4249f48
Jerry <50892930+lsjsos@users.noreply.github.com>
2023-10-26 16:36:02 +0900

    Intro Scene #Throwing1

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    private int count = 0;
+
+    public GameObject throw_object;
+    public AudioSource aus;
+    public AudioClip paper;
+
+    private void Start()
+    {
+        aus = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && count == 0)
+        {
+            throw_object.SetActive(true);
+            throw_object.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 400
+                * Time.deltaTime, ForceMode.Impulse);
+            aus.clip = paper;
+            aus.PlayDelayed(0.5f);
+            count++;
+        }
+    }
+}
